3a9fed0627d9ad1167a6071160709008233c6c7b
fandeng <455817156@qq.com>
2018-10-24 11:22:37 +0800

    登录模块，测试

---
+++ Example/build/Pods.build/Debug-iphoneos/Pods-Login_Tests.build/DerivedSources/Pods_Login_Tests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_Login_TestsVersionString[];
+ extern const double Pods_Login_TestsVersionNumber;
+
+ const unsigned char Pods_Login_TestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_Login_Tests  PROJECT:Pods-1" "\n";
+ const double Pods_Login_TestsVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ LoginViewController.h
@@ -0,0 +1,12 @@
+//
+//  LoginViewController.h
+//  Login
+//
+//  Created by 樊登 on 2018/10/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
